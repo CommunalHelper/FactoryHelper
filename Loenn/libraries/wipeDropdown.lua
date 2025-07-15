e2238361c056dd7529b8e202f93edff701aded79
SilverDorian46 <86711559+SilverDorian46@users.noreply.github.com>
2025-07-15 17:37:51 +0400

    Add the Gear Wipe to the dropdown list

---
+++ Loenn/libraries/wipeDropdown.lua
@@ -0,0 +1,5 @@
+local celesteEnums = require("consts.celeste_enums")
+
+-- Adds a Screen Wipe to the dropdown list.
+
+celesteEnums.wipe_names["Gear [Factory Helper]"] = "FactoryHelper.ScreenWipes.GearWipe"
